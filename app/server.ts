166a760db12fe477de092bea37fad8af2570e2b4
Domingues <domingues.fjp@hotmail.com>
2024-04-16 17:40:52 +0100

    Added global middlewares

--- app/server.ts
+++ app/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import helmet from "helmet";
 
 //import dbInit from "./db/db.init";
 
@@ -8,9 +9,19 @@ const port = process.env.PORT || 3000;
 
 //dbInit();
 
+// Global Middlewares
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
+
+// End Global Middlewares
+
 // Simple Route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.status(200).send({"message": "Hello World"});
 });
 
 // Start the server
