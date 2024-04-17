e0cd5e050de3f55592886958e2634b63de340e96
Domingues <domingues.fjp@hotmail.com>
2024-04-17 14:25:04 +0100

    Added async

--- app/database/init.ts
+++ app/database/init.ts
@@ -3,9 +3,8 @@ import sequelizeConnection from "./db.config"
 const isDev = process.env.NODE_ENV === "development";
 const isTest = process.env.NODE_ENV !== "test";
 
-export const initDatabase = () => {
-    sequelizeConnection.sync({alter: isDev || isTest}).then(() => {
-        console.log("Database synced");
-        console.log("Enviroment: ", process.env.NODE_ENV);
-    })
-}
+export const initDatabase = async () => {
+    await sequelizeConnection.sync({ alter: isDev || isTest });
+    console.log("Database synced");
+    console.log("Environment: ", process.env.NODE_ENV);
+};
