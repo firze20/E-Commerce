3eb8e095117d6b1d927be3e087ea6b3408e39417
Domingues <domingues.fjp@hotmail.com>
2024-04-17 16:30:48 +0100

    simplified

--- app/routes/main.routes.ts
+++ app/routes/main.routes.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response} from "express";
+import { Request, Response, Router} from "express";
 
-const mainRouter = express.Router();
+const mainRouter = Router();
 
 mainRouter.get("/", (req: Request, res: Response) => {
     res.status(200).send({"message": "Welcome to E-Commerce API!"});
