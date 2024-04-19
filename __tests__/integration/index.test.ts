682a68fe86f6774028a097436c7fa7d31f56ac0f
Domingues <domingues.fjp@hotmail.com>
2024-04-20 00:15:51 +0100

    Adding index tests

--- __tests__/integration/index.test.ts
+++ __tests__/integration/index.test.ts
@@ -1,20 +1,19 @@
 
 import Item from "../../app/database/models/Item";
 import Role from "../../app/database/models/Role";
+import User from "../../app/database/models/User";
 import {connectDatabase, closeDatabase} from "../../app/utils/connect";
 
 const dbTeardown = async () => {
   await Role.destroy({ cascade: true, truncate: true, force: true });
+  await Item.destroy({ cascade: true, truncate: true, force: true });
+  await User.destroy({ cascade: true, truncate: true, force: true });
   await closeDatabase();
 };
 
 describe("Test Database Models", () => {
   beforeAll(async () => {
     await connectDatabase(); // Initialize database connection
-  }, 20000); // Adding 20 seconds because it needs to resync all models
-
-  beforeEach(async () => {
-    jest.clearAllMocks();
 
     // Create sample role records
     await Promise.all([
@@ -34,16 +33,24 @@ describe("Test Database Models", () => {
         description:
           "Can buy, manage items and stocks, and manage users in the database",
       }),
-      // List items
-      // Item.create({
-      //   id: 1,
-      //   name: "Box of Pencils",
-      //   description: "A box of 100 pencils",
-      //   price: 2.50,
-      //   image: "https://e7.pngegg.com/pngimages/702/527/png-clipart-colored-pencil-crayon-boxed-color-pencil-png-material-color-splash.png",
-      // })
+      Item.create({
+        id: 1,
+        name: "Box of Pencils",
+        description: "A box of 100 pencils",
+        price: 2.50,
+        image: "https://e7.pngegg.com/pngimages/702/527/png-clipart-colored-pencil-crayon-boxed-color-pencil-png-material-color-splash.png",
+      })
     ]);
-  });
+
+
+  }, 25000); // Adding a timeout of 25 seconds
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    
+    // Probably won't use it 
+    
+  }); 
 
   it("Test Role Creation and Getter", async () => {
     // Mock response for findOne method
@@ -61,6 +68,33 @@ describe("Test Database Models", () => {
     });
     expect(result?.description).toEqual(mockResponse.description);
   });
+
+  it("Test if item corresponds to the following model", async () => {
+    const mockResponse = {
+      id: 1,
+      name: "Box of Pencils",
+      description: "A box of 100 pencils",
+      price: 2.50,
+    }
+
+    const result = await Item.findOne({
+      where: { name: "Box of Pencils" },
+    });
+
+    expect(result?.id).toBe(mockResponse.id);
+  });
+
+  it("Test if user creation generates bcrypt for passwords", async () => {
+    const user = await User.create({
+      id: 1,
+      username: "Test User",
+      password: "password300",
+      email: "test@gmail.com",
+      roles: [1,2,3],
+    });
+
+    expect(user?.password).not.toBe("password300");
+  })
 
   afterAll(async () => {
     await dbTeardown(); // Clean up database after all tests
