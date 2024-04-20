f6900e2a8b858dc782790f8a16da1ff17be3da8a
Domingues <domingues.fjp@hotmail.com>
2024-04-20 15:52:01 +0100

    Added passport local strategy

---
+++ app/config/passportLocalStrategy.ts
@@ -0,0 +1,18 @@
+import User from "../database/models/User";
+import passport from "passport";
+
+import { Strategy as LocalStrategy } from "passport-local";
+
+passport.use(new LocalStrategy(
+    async function (username, password, done) {
+        try {
+            const user = await User.findOne({ where: { username: username}})
+            if(!user) {
+                return done(null, false, { message: "Incorrect username"})
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }
+))
