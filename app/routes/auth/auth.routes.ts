a749b2f6044271dfff7d8df970fe67d7e3ce2d03
Domingues <domingues.fjp@hotmail.com>
2024-05-01 23:43:48 +0100

    added comment

--- app/routes/auth/auth.routes.ts
+++ app/routes/auth/auth.routes.ts
@@ -2,6 +2,8 @@ import {Request, Response, Router} from "express";
 
 const authRouter = Router();
 
+
+//We need to use passport strategy 
 authRouter.post("/signup", (req: Request, res: Response) => {
     res.status(201).send({
         message: "User created!"
