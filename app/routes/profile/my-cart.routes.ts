f50d969347fbbc3b623d50279eba17f6af35e5a3
Domingues <domingues.fjp@hotmail.com>
2024-05-28 23:47:09 +0100

    working req.user

--- app/routes/profile/my-cart.routes.ts
+++ app/routes/profile/my-cart.routes.ts
@@ -4,7 +4,7 @@ import { authenticateJwt } from "../../middlewares/passport/passportJWT";
 const myCartRouter = Router();
 
 myCartRouter.get("/", authenticateJwt, (req: any, res: any) => {
-  res.status(200).send({ message: "Welcome to My Cart" });
+  res.status(200).send({ message: `Welcome to your Cart ${req.user.username}!` });;
 });
 
 export default myCartRouter;
