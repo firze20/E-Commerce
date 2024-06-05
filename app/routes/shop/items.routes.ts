5b190e5140201ae47808b2823679b19ba6c74a07
Domingues <domingues.fjp@hotmail.com>
2024-06-05 21:19:37 +0100

    added openapi

--- app/routes/shop/items.routes.ts
+++ app/routes/shop/items.routes.ts
@@ -168,7 +168,105 @@ shopRouter.get("/", getItemsFromStoreController);
  *                   example: "Error getting item"
  */
 shopRouter.get("/item/:id", getItemController);
-
+/**
+ * @openapi
+ * /api/e-commerce/store/item:
+ *   post:
+ *     tags:
+ *       - Items
+ *     description: "Create a new item in the store."
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: "Name of the item"
+ *                 example: "Sandals"
+ *               description:
+ *                 type: string
+ *                 description: "Description of the item"
+ *                 example: "A pair of sandals to walk on the beach"
+ *               price:
+ *                 type: number
+ *                 format: float
+ *                 description: "Price of the item"
+ *                 example: 12.99
+ *               image:
+ *                 type: string
+ *                 description: "URL of the item image"
+ *                 example: "https://e7.pngegg.com/pngimages/996/985/png-clipart-sandals-sandals.png"
+ *               stockId:
+ *                 type: integer
+ *                 description: "ID of the stock associated with the item"
+ *                 example: 7
+ *     responses:
+ *       201:
+ *         description: "Item created successfully"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Item created!"
+ *                 item:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 7
+ *                     name:
+ *                       type: string
+ *                       example: "Sandals"
+ *                     description:
+ *                       type: string
+ *                       example: "A pair of sandals to walk on the beach"
+ *                     price:
+ *                       type: number
+ *                       format: float
+ *                       example: 12.99
+ *                     image:
+ *                       type: string
+ *                       example: "https://e7.pngegg.com/pngimages/996/985/png-clipart-sandals-sandals.png"
+ *                     stockId:
+ *                       type: integer
+ *                       example: 7
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       example: "2024-06-05T20:13:32.724Z"
+ *                     updatedAt:
+ *                       type: string
+ *                       format: date-time
+ *                       example: "2024-06-05T20:13:32.988Z"
+ *       401:
+ *         description: "Unauthorized"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized"
+ *       500:
+ *         description: "Error creating item"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Error creating item"
+ */
 shopRouter.post("/item", [authenticateJwt, isAdmin], createItemController);
 
 shopRouter.delete(
