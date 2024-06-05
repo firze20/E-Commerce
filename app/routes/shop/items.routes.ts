5fb5a078e03f54f06dc0bca364ef57e0147e767e
Domingues <domingues.fjp@hotmail.com>
2024-06-05 21:08:39 +0100

    added item routes

--- app/routes/shop/items.routes.ts
+++ app/routes/shop/items.routes.ts
@@ -97,7 +97,76 @@ const shopRouter = Router();
  *                   example: "Error getting items"
  */
 shopRouter.get("/", getItemsFromStoreController);
-
+/**
+ * @openapi
+ * /api/e-commerce/store/item/{id}:
+ *   get:
+ *     tags:
+ *       - Items
+ *     description: "Get a specific item by ID from the store."
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: "ID of the item"
+ *     responses:
+ *       200:
+ *         description: "Retrieve item by ID"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 item:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 3
+ *                     name:
+ *                       type: string
+ *                       example: "Steam Deck"
+ *                     description:
+ *                       type: string
+ *                       example: "A handheld gaming console"
+ *                     price:
+ *                       type: number
+ *                       format: float
+ *                       example: 399
+ *                     image:
+ *                       type: string
+ *                       example: "https://clan.akamai.steamstatic.com/images//39049601/a1aa0624727ea6fd61bd179d214eaca1904fae45.png"
+ *                     stock:
+ *                       type: integer
+ *                       example: 1
+ *                     categories:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                         example: "Undefined"
+ *       404:
+ *         description: "Item not found"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Item not found"
+ *       500:
+ *         description: "Server error"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Error getting item"
+ */
 shopRouter.get("/item/:id", getItemController);
 
 shopRouter.post("/item", [authenticateJwt, isAdmin], createItemController);
