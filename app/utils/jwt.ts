c2145a82eaac7026ccaf4374323e771bc8b65c0f
Domingues <domingues.fjp@hotmail.com>
2024-06-15 13:20:57 +0100

    Added jsdocs to jwt utility function

--- app/utils/jwt.ts
+++ app/utils/jwt.ts
@@ -11,26 +11,46 @@ const jwtSecret = process.env.JWT_SECRET as string;
 const refreshTokenSecret = process.env.REFRESH_SECRET as string;
 
 
-// Generate Token
+/**
+ * Generates a JWT access token for a user.
+ * @param {User} user - The user object.
+ * @param {string[]} roles - Array of roles associated with the user.
+ * @returns {string} The generated JWT access token.
+ */
 const generateToken = (user: User, roles: string[]) => {
     const token = jwt.sign({ id: user.id, roles}, jwtSecret, { expiresIn: "1h" });
     return token;
 };
 
-// Generate Refresh Token
+/**
+ * Generates a JWT refresh token for a user.
+ * @param {User} user - The user object.
+ * @returns {string} The generated JWT refresh token.
+ */
 const generateRefreshToken = (user: User) => {
     const refreshToken = jwt.sign({ id: user.id }, refreshTokenSecret, { expiresIn: "7d" });
     return refreshToken;
 };
 
-// Decode Refresh Token
+/**
+ * Decodes and verifies a JWT refresh token.
+ * @param {string} refreshToken - The JWT refresh token to decode and verify.
+ * @returns {string | jwt.JwtPayload} The decoded JWT payload.
+ * @throws {jwt.JsonWebTokenError} If the token is invalid.
+ * @throws {jwt.TokenExpiredError} If the token has expired.
+ * @throws {jwt.NotBeforeError} If the token is not active yet.
+ */
 const decodeJwtRefreshToken = (refreshToken: string) => {
     console.log(refreshToken);
     console.log(refreshTokenSecret);
     return jwt.verify(refreshToken, refreshTokenSecret);
 };
 
-// Type Guard check if its an object and not a string for RefreshToken
+/**
+ * Type guard to check if the decoded JWT is a payload object.
+ * @param {string | jwt.JwtPayload} decoded - The decoded JWT.
+ * @returns {decoded is jwt.JwtPayload} True if the decoded JWT is a payload object, false otherwise.
+ */
 const isJwtPayload = (decoded: string | jwt.JwtPayload): decoded is jwt.JwtPayload => {
     return typeof decoded === 'object' && decoded !== null;
   };
