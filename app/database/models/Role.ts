1b359c0fe4a258b5ad9e1fed1fe565ad72d0aba4
Domingues <domingues.fjp@hotmail.com>
2024-06-16 10:00:27 +0100

    Adding jsDocs

--- app/database/models/Role.ts
+++ app/database/models/Role.ts
@@ -1,40 +1,68 @@
-import { Table, Column, Model, DataType, CreatedAt, UpdatedAt, BeforeCreate, HasOne, AutoIncrement, BelongsTo, ForeignKey, PrimaryKey, NotNull, BelongsToMany } from "sequelize-typescript";
+import { Table, Column, Model, DataType, CreatedAt, UpdatedAt, BelongsToMany } from "sequelize-typescript";
 import UserRole from "./UserRoles";
 import User from "./User";
 
+/**
+ * Represents a role that can be assigned to users.
+ * @class Role
+ * @extends {Model<Role>}
+ */
 @Table({
     tableName: "roles",
     timestamps: true
 })
-
-class Role extends Model {
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.INTEGER)
+class Role extends Model<Role> {
+    /**
+     * The unique identifier for the role.
+     * @type {number}
+     */
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    })
     id!: number;
 
+    /**
+     * The name of the role.
+     * @type {string}
+     */
     @Column({
         type: DataType.STRING,
-        allowNull: false
+        allowNull: false,
     })
     name!: string;
 
+    /**
+     * The description of the role.
+     * @type {string}
+     */
     @Column({
         type: DataType.STRING,
-        allowNull: false
+        allowNull: false,
     })
     description!: string;
 
+    /**
+     * The timestamp when this role was created.
+     * @type {Date}
+     */
     @CreatedAt
     createdAt!: Date;
 
+    /**
+     * The timestamp when this role was last updated.
+     * @type {Date}
+     */
     @UpdatedAt
     updatedAt!: Date;
 
+    /**
+     * The users associated with this role.
+     * @type {User[]}
+     */
     @BelongsToMany(() => User, () => UserRole)
-    user!: User[];
+    users!: User[];
 }
-
-
 
 export default Role;
--- app/database/models/Stock.ts
+++ app/database/models/Stock.ts
@@ -1,37 +1,60 @@
 import { Table, Column, Model, DataType, CreatedAt, UpdatedAt, BeforeCreate, HasOne, AutoIncrement, ForeignKey, NotNull } from "sequelize-typescript";
 import Item from "./Item";
-
+/**
+ * Represents the stock of items in the inventory.
+ * @class Stock
+ * @extends {Model<Stock>}
+ */
 @Table({
     tableName: "stocks",
     timestamps: true
 })
 class Stock extends Model {
+       /**
+     * The unique identifier for the stock entry.
+     * @type {number}
+     */
     @AutoIncrement
     @Column({
         type: DataType.INTEGER,
         primaryKey: true
     })
     id!: number;
-
+    /**
+     * The quantity of items available in stock.
+     * @type {number}
+     */
     @Column({
         type: DataType.INTEGER,
         defaultValue: 0
     })
     quantity!: number
-
+     /**
+     * The ID of the item associated with this stock entry.
+     * @type {number}
+     */
     @ForeignKey(() => Item)
     @Column({
         type: DataType.INTEGER,
         allowNull: false
     })
     itemId!: number;
-
+    /**
+     * The item associated with this stock entry.
+     * @type {Item}
+     */
     @HasOne(() => Item)
     declare item: Item;
-
+    /**
+     * The timestamp when this stock entry was created.
+     * @type {Date}
+     */
     @CreatedAt
     createdAt!: Date;
-
+    /**
+     * The timestamp when this stock entry was last updated.
+     * @type {Date}
+     */
     @UpdatedAt
     updatedAt!: Date;
 }
