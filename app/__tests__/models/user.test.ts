ec5bf7f9253e79016ebfb9be9b1d5621a8872c0b
Domingues <domingues.fjp@hotmail.com>
2024-06-18 00:44:23 +0100

    updated user.test

--- app/__tests__/models/user.test.ts
+++ app/__tests__/models/user.test.ts
@@ -2,13 +2,17 @@ import exp from "constants";
 import User from "../../database/models/User";
 
 describe("Test User model", () => {
-  test("Test User model", async () => {
+  test("Test User creation, password shouldn't be the same as the payload", async () => {
+    const password = "megaPassword"
+
     const user = await User.create({
       username: "test_number_1",
       email: "email@yahoo.com",
-      password: "megaPassword",
+      password: password,
     });
 
     expect(user.username).toBe("test_number_1");
+
+    expect(user.password).not.toBe(password)
 })
 })
