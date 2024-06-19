9f08d11f036743061c1a441b9baf8d35a4d27908
Domingues <domingues.fjp@hotmail.com>
2024-06-19 12:24:33 +0100

    item creation

--- app/__tests__/models/item.test.ts
+++ app/__tests__/models/item.test.ts
@@ -1,16 +1,19 @@
 import Item from "../../database/models/Item";
 import Stock from "../../database/models/Stock";
 
+let item: Item;
+
 describe("Test Item model", () => {
-    test("When an item is created, a stock id of that item should exist", async () => {
-        // Create an item
-        const item = await Item.create({
+    beforeAll(async () => {
+        item = await Item.create({
             name: "Pikachu Plushie Doll",
             description: "A pokemon plushie doll",
             price: 4.99,
             image: "https://www.pokemoncenter.com/images/DAMRoot/High/10000/P7730_701-29240_02.jpg"
         });
+    });
 
+    test("When an item is created, a stock id of that item should exist", async () => {
         // Find the stock of the item
         const stock = await Stock.findOne({
             where: {
@@ -19,5 +22,17 @@ describe("Test Item model", () => {
         });
 
         expect(stock).not.toBeNull();
-    })
-})
+    });
+
+    test("When an item is created, the stock quantity should be 1", async () => {
+        // Find the stock of the item
+        const stock = await Stock.findOne({
+            where: {
+                itemId: item.id
+            }
+        });
+
+        expect(stock?.quantity).toBe(1);
+    });
+});
+
