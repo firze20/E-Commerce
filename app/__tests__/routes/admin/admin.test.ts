609272a8988e11a9b822bdbd7f931aa808cf7552
Domingues <domingues.fjp@hotmail.com>
2024-06-20 12:02:18 +0100

    adding admin tests

--- app/__tests__/routes/admin/admin.test.ts
+++ app/__tests__/routes/admin/admin.test.ts
@@ -1,6 +1,23 @@
+import request from "supertest";
 
-describe("Test admin endpoints", () => {
-    test("Simple test", () => {
-        expect(1).toBe(1);
+const app = global.__APP__;
+
+describe("Admin Endpoints", () => {
+    it("Get all users", async () => {
+        const response = await request(app).get("/api/e-commerce/admin/users");
+        expect(response.status).toBe(200);
+        expect(response.body.users).toBeDefined();
     });
+
+    it("Get a single user", async () => {
+        const response = await request(app).get("/api/e-commerce/admin/users/1");
+        expect(response.status).toBe(200);
+        expect(response.body.user).toBeDefined();
+    });
+
+    it("Remove a user", async () => {
+        const response = await request(app).delete("/api/e-commerce/admin/users/3");
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("User removed successfully");
+    })
 });
