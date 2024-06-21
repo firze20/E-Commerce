610bf1c24aea757ee864117f6802e13696814dc6
Domingues <domingues.fjp@hotmail.com>
2024-06-21 09:31:48 +0100

    Adding cors

--- app/utils/server.ts
+++ app/utils/server.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import passport from "passport";
 import morgan from "morgan";
+import cors, { CorsOptions } from "cors";
 //Routes
 import mainRouter from "../routes";
 
@@ -17,26 +18,32 @@ const isDev = process.env.NODE_ENV === "development";
  */
 async function createServer() {
 
+  const corsOptions: CorsOptions = {
+    origin: "http://localhost:3000",
+  };
+
   await connectDatabase();
 
   const app = express();
 
   // Global Middlewares
 
-  app.use(express.json());
+  app.use(cors(corsOptions)); // Enable CORS with specific options
+
+  app.use(express.json()); // Parse JSON payloads
 
   // Cookie Parser
 
-  app.use(cookieParser());
+  app.use(cookieParser()); // Parse Cookie header and populate req.cookies
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true })); // Parse URL-encoded payloads
 
-  app.use(helmet()); // to hide the type of application
+  app.use(helmet()); // Set security-related HTTP headers
 
-  app.use(morgan(isDev ? "dev" : "tiny"));
+  app.use(morgan(isDev ? "dev" : "tiny")); // Log HTTP requests
 
-  //passport middleware
-  app.use(passport.initialize()); // helps secure endpoints
+  // Passport middleware
+  app.use(passport.initialize()); // Initialize Passport for authentication
 
   // End Global Middlewares
 
