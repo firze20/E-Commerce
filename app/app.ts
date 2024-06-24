20a8ce51a15166f9a4ca60aa3495d087a0040539
Domingues <domingues.fjp@hotmail.com>
2024-06-24 13:05:58 +0100

    app using cluster model

--- app/app.ts
+++ app/app.ts
@@ -1,23 +1,38 @@
 import logger from "./utils/logger";
 import createServer from "./utils/server";
 import swaggerDocs from "./utils/swagger";
+import cluster from "cluster";
+import os from "os";
 
-const port = Number(process.env.PORT)  || 3000;
+const numCpus = os.cpus().length;
+const port = Number(process.env.PORT) || 3000;
 
+if (cluster.isPrimary) {
+  logger.info(`Primary ${process.pid} is running`);
 
-// Start the server async so it can sync the database 
-(async () => {
-  try {
-    const app = await createServer();
-
-    app.listen(port, () => {
-      logger.info(`Server running on port ${port}`);
-      
-      // Passing Swagger docs to app
-      swaggerDocs(app, port);
-    });
-  } catch (error) {
-    logger.error('Error starting the server:', error);
-    process.exit(1); // Exit the process with an error code
+  // Fork workers.
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
   }
-})();
+
+  cluster.on('exit', (worker, code, signal) => {
+    logger.info(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    logger.info('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case, it is an HTTP server
+  (async () => {
+    try {
+      const app = await createServer();
+      app.listen(port, () => {
+        logger.info(`Worker ${process.pid} started: Server running on port ${port}`);
+        swaggerDocs(app, port);
+      });
+    } catch (error) {
+      logger.error('Error starting the server:', error);
+      process.exit(1);
+    }
+  })();
+}
