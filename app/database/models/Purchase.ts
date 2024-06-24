ad49409804ebd82b7673aac5649bda9a0594c8e8
Domingues <domingues.fjp@hotmail.com>
2024-06-24 23:18:03 +0100

    lets make purchases

--- app/database/models/Purchase.ts
+++ app/database/models/Purchase.ts
@@ -1,10 +1,22 @@
-import { Table, Column, Model, DataType, CreatedAt, ForeignKey, AutoIncrement, BelongsToMany, PrimaryKey, BelongsTo } from "sequelize-typescript";
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  CreatedAt,
+  ForeignKey,
+  AutoIncrement,
+  BelongsToMany,
+  PrimaryKey,
+  BelongsTo,
+} from "sequelize-typescript";
 import User from "./User";
 import Item from "./Item";
 import Cart from "./Cart";
 import PurchaseItem from "./PurchaseItem";
 import sequelize from "../db.config";
 import logger from "../../utils/logger";
+import Stock from "./Stock";
 
 /**
  * Represents an Purchase in the database.
@@ -12,111 +24,135 @@ import logger from "../../utils/logger";
  * @extends {Model}
  */
 @Table({
-    tableName: "purchases"
+  tableName: "purchases",
 })
 class Purchase extends Model {
-    /**
-     * The unique identifier for the purchase.
-     * @type {number}
-     */
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.INTEGER)
-    id!: number;
+  /**
+   * The unique identifier for the purchase.
+   * @type {number}
+   */
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
 
-    /**
-     * The date when the purchase was made.
-     * @type {Date}
-     */
-    @Column({
-        type: DataType.DATE
-    })
-    date!: Date;
+  /**
+   * The date when the purchase was made.
+   * @type {Date}
+   */
+  @Column({
+    type: DataType.DATE,
+  })
+  date!: Date;
 
-      /**
-     * The items purchased in this purchase.
-     * @type {Item[]}
-     */
-      @BelongsToMany(() => Item, () => PurchaseItem)
-      items!: Item[];
+  /**
+   * The items purchased in this purchase.
+   * @type {Item[]}
+   */
+  @BelongsToMany(() => Item, () => PurchaseItem)
+  items!: Item[];
 
-    /**
-     * The foreign key for the user who made the purchase.
-     * @type {number}
-     */
-    @ForeignKey(() => User)
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false
-    })
-    userId!: number;
+  /**
+   * The foreign key for the user who made the purchase.
+   * @type {number}
+   */
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  userId!: number;
 
-    @BelongsTo(() => User)
-    user!: User;
+  @BelongsTo(() => User)
+  user!: User;
 
-    @ForeignKey(() => Cart)
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false
-    })
-    cartId!: number;
+  @ForeignKey(() => Cart)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  cartId!: number;
 
-    @BelongsTo(() => Cart)
-    cart!: Cart;
+  @BelongsTo(() => Cart)
+  cart!: Cart;
 
-    @Column({
-        type: DataType.DECIMAL(10, 2), // Adjust precision and scale as needed
-        allowNull: false,
-        defaultValue: 0.0,
-    })
-    totalPrice!: number; // Total price of the purchase
+  @Column({
+    type: DataType.DECIMAL(10, 2), // Adjust precision and scale as needed
+    allowNull: false,
+    defaultValue: 0.0,
+  })
+  totalPrice!: number; // Total price of the purchase
 
-    // Create purchase
-    /**
-     * Creates a new purchase in the database.
-     * @param cart 
-     */
-    static async createPurchase(cart: Cart): Promise<void> {
-        const transaction = await sequelize.transaction();
-        try {
-            // Get total price from the cart to calculate the price
-            const totalPrice = await cart.getTotalPrice();
-            // Gets all Items in the cart
-            const totalItems = await cart.getCartItems();
-    
-            // Create a new purchase
-            const purchase = await Purchase.create({
-                date: new Date(),
-                userId: cart.userId,
-                cartId: cart.id,
-                totalPrice
-            }, { transaction });
-    
-            for (const cart of totalItems) {
-                await PurchaseItem.create({
-                    purchaseId: purchase.id,
-                    itemId: cart.item.id,
-                    price: cart.item.price
-                }, { transaction });
-            }
-    
-            await transaction.commit();
-        } catch (error: any) {
-            await transaction.rollback();
-            logger.error("Error creating purchase: ", error);
-            throw new Error("Error creating purchase");
+  // Create purchase
+  /**
+   * Creates a new purchase in the database.
+   * @param cart
+   */
+  static async createPurchase(cart: Cart): Promise<void> {
+    const transaction = await sequelize.transaction();
+    try {
+      // Get total price from the cart to calculate the price
+      const totalPrice = await cart.getTotalPrice();
+      // Gets all Items in the cart
+      const totalItems = await cart.getCartItems();
+
+      // Create a new purchase
+      const purchase = await Purchase.create(
+        {
+          date: new Date(),
+          userId: cart.userId,
+          cartId: cart.id,
+          totalPrice,
+        },
+        { transaction }
+      );
+
+      for (const cartItem of totalItems) {
+        // Check if there is item stock available
+
+        const stock = await Stock.findOne({
+          where: { itemId: cartItem.item.id },
+          transaction,
+          lock: transaction.LOCK.UPDATE, // Lock the row to prevent race conditions, lock any updates 
+        });
+
+        if (!stock || stock.quantity < cartItem.quantity) {
+          throw new Error(`Not enough stock for item: ${cartItem.item.name}`);
         }
-    }
 
-    // Get Purchase Items
-    /**
-     * Gets the items in the purchase.
-     * @returns {Promise<Item[]>}
-     */
-    async getPurchaseItems(): Promise<Item[]> {
-        const items = await this.$get("items") as Item[];
-        return items;
+        await PurchaseItem.create(
+          {
+            purchaseId: purchase.id,
+            itemId: cartItem.item.id,
+            quantity: cartItem.quantity,
+            price: cartItem.item.price,
+          },
+          { transaction }
+        );
+
+        // Decrease the item stock quantity
+
+        stock.quantity -= cartItem.quantity;
+        await stock.save({ transaction })
+      }
+
+      await transaction.commit();
+    } catch (error: any) {
+      await transaction.rollback();
+      logger.error("Error creating purchase: ", error);
+      throw new Error("Error creating purchase");
     }
+  }
+
+  // Get Purchase Items
+  /**
+   * Gets the items in the purchase.
+   * @returns {Promise<Item[]>}
+   */
+  async getPurchaseItems(): Promise<Item[]> {
+    const items = (await this.$get("items")) as Item[];
+    return items;
+  }
 }
 
 export default Purchase;
--- app/database/models/PurchaseItem.ts
+++ app/database/models/PurchaseItem.ts
@@ -1,4 +1,11 @@
-import { Table, Column, Model, DataType, ForeignKey, BelongsTo } from "sequelize-typescript";
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+  BelongsTo,
+} from "sequelize-typescript";
 import Purchase from "./Purchase";
 import Item from "./Item";
 
@@ -9,46 +16,46 @@ import Item from "./Item";
  * @extends {Model}
  */
 @Table({
-    tableName: "purchase_items",
+  tableName: "purchase_items",
 })
 class PurchaseItem extends Model {
-    /**
-     * The foreign key for the purchase associated with this item.
-     * @type {number}
-     */
-    @ForeignKey(() => Purchase)
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false
-    })
-    purchaseId!: number;
+  /**
+   * The foreign key for the purchase associated with this item.
+   * @type {number}
+   */
+  @ForeignKey(() => Purchase)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  purchaseId!: number;
 
-    /**
-     * The Purchase model instance associated with this item.
-     * @type {Purchase}
-     */
-    @BelongsTo(() => Purchase)
-    purchase!: Purchase;
+  /**
+   * The Purchase model instance associated with this item.
+   * @type {Purchase}
+   */
+  @BelongsTo(() => Purchase)
+  purchase!: Purchase;
 
-    /**
-     * The foreign key for the item associated with this purchase.
-     * @type {number}
-     */
-    @ForeignKey(() => Item)
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false
-    })
-    itemId!: number;
+  /**
+   * The foreign key for the item associated with this purchase.
+   * @type {number}
+   */
+  @ForeignKey(() => Item)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  itemId!: number;
 
-    /**
-     * The Item model instance associated with this purchase.
-     * @type {Item}
-     */
-    @BelongsTo(() => Item)
-    item!: Item;
+  /**
+   * The Item model instance associated with this purchase.
+   * @type {Item}
+   */
+  @BelongsTo(() => Item)
+  item!: Item;
 
-      /**
+  /**
    * The quantity of items in the Cart.
    * @type {number}
    */
@@ -61,6 +68,16 @@ class PurchaseItem extends Model {
     },
   })
   quantity!: number;
+
+  /**
+   * The price of the item at the time of purchase.
+   * @type {number}
+   */
+  @Column({
+    type: DataType.DECIMAL(10, 2), 
+    allowNull: false,
+  })
+  price!: number;
 }
 
 export default PurchaseItem;
