9dc71a05e2ad48436e6b2d8fbd49c41020c92ea1
Domingues <domingues.fjp@hotmail.com>
2024-07-01 15:42:08 +0100

    Added JsDocs OpenAPI Documentation

--- app/routes/manager/routes/category.routes.ts
+++ app/routes/manager/routes/category.routes.ts
@@ -6,12 +6,256 @@ const categoryRouter = Router();
 
 
 // Create a new category
+/**
+ * @openapi
+ * /api/e-commerce/manager/category:
+ *  post:
+ *    tags:
+ *      - Manager
+ *    description: "Create a new category."
+ *    security:
+ *      - cookieAuth: []
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                description: "Name of the category."
+ *                example: "Movies"
+ *              description:
+ *                type: string
+ *                description: "Description of the category."
+ *                example: "Digital and Physical movies!"
+ *    responses:
+ *      201:
+ *        description: Category created successfully.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Category created"
+ *                category:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: integer
+ *                      example: 1
+ *                    name:
+ *                      type: string
+ *                      example: "Movies"
+ *                    description:
+ *                      type: string
+ *                      example: "Digital and Physical movies!"
+ *                    createdAt:
+ *                      type: string
+ *                      format: date-time
+ *                    updatedAt:
+ *                      type: string
+ *                      format: date-time
+ *      401:
+ *        description: Unauthorized access.
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string
+ *              example: "Unauthorized"
+ *      403:
+ *        description: Forbidden.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "You don't have permission to access this route"
+ *      500:
+ *        description: Server error.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Failed to create category"
+ */
 categoryRouter.post('/', [authenticateJwt, isManager], postCategoryController);
 
 // Edit a category
+/**
+ * @openapi
+ * /api/e-commerce/manager/category/{id}:
+ *  put:
+ *    tags:
+ *      - Manager
+ *    description: "Update an existing category."
+ *    security:
+ *      - cookieAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: The ID of the category to update
+ *        schema:
+ *          type: integer
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                description: "Name of the category."
+ *                example: "Movies"
+ *              description:
+ *                type: string
+ *                description: "Description of the category."
+ *                example: "Digital and Physical movies!"
+ *    responses:
+ *      200:
+ *        description: Category updated successfully.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Category updated"
+ *                category:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: integer
+ *                      example: 1
+ *                    name:
+ *                      type: string
+ *                      example: "Movies"
+ *                    description:
+ *                      type: string
+ *                      example: "Digital and Physical movies!"
+ *                    createdAt:
+ *                      type: string
+ *                      format: date-time
+ *                    updatedAt:
+ *                      type: string
+ *                      format: date-time
+ *      401:
+ *        description: Unauthorized access.
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string
+ *              example: "Unauthorized"
+ *      403:
+ *        description: Forbidden.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "You don't have permission to access this route"
+ *      404:
+ *        description: Category not found.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Category not found"
+ *      500:
+ *        description: Server error.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Failed to update category"
+ */
 categoryRouter.put('/:id', [authenticateJwt, isManager], editCategoryController);
 
 // Delete a category
+/**
+ * @openapi
+ * /api/e-commerce/manager/category/{id}:
+ *  delete:
+ *    tags:
+ *      - Manager
+ *    description: "Delete an existing category."
+ *    security:
+ *      - cookieAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: The ID of the category to delete
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      204:
+ *        description: Category deleted successfully.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Category deleted"
+ *      401:
+ *        description: Unauthorized access.
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string
+ *              example: "Unauthorized"
+ *      403:
+ *        description: Forbidden.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "You don't have permission to access this route"
+ *      404:
+ *        description: Category not found.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Category not found"
+ *      500:
+ *        description: Server error.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Failed to delete category"
+ */
 categoryRouter.delete('/:id', [authenticateJwt, isManager], removeCategoryController);
 
 
--- app/routes/manager/routes/stock.routes.ts
+++ app/routes/manager/routes/stock.routes.ts
@@ -12,8 +12,160 @@ import {
 
 const stockRouter = Router();
 
+/**
+ * @openapi
+ * /api/e-commerce/manager/stock/{id}/add:
+ *  post:
+ *    tags:
+ *      - Manager
+ *    description: "Add stock to an item."
+ *    security:
+ *      - cookieAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: The ID of the item to add stock to
+ *        schema:
+ *          type: integer
+ *    requestBody:
+ *      required: false
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              quantity:
+ *                type: integer
+ *                description: The quantity of stock to add
+ *                example: 10
+ *    responses:
+ *      200:
+ *        description: Stock added successfully.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Stock added successfully"
+ *      401:
+ *        description: Unauthorized access.
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string
+ *              example: "Unauthorized"
+ *      403:
+ *        description: Forbidden.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "You don't have permission to access this route"
+ *      404:
+ *        description: Item not found.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Item not found"
+ *      500:
+ *        description: Server error.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "An error occurred while adding stock"
+ */
 stockRouter.post("/:id/add", authenticateJwt, isManager, addStockController);
 
+/**
+ * @openapi
+ * /api/e-commerce/manager/stock/{id}/remove:
+ *  post:
+ *    tags:
+ *      - Manager
+ *    description: "Remove stock to an item."
+ *    security:
+ *      - cookieAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: The ID of the item to add stock to
+ *        schema:
+ *          type: integer
+ *    requestBody:
+ *      required: false
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              quantity:
+ *                type: integer
+ *                description: The quantity of stock to add
+ *                example: 10
+ *    responses:
+ *      200:
+ *        description: Stock added successfully.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Successfully decreased item stock"
+ *      401:
+ *        description: Unauthorized access.
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string
+ *              example: "Unauthorized"
+ *      403:
+ *        description: Forbidden.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "You don't have permission to access this route"
+ *      404:
+ *        description: Item not found.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Item not found"
+ *      500:
+ *        description: Server error.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "An error occurred while removing stock"
+ */
 stockRouter.post("/:id/remove", authenticateJwt, isManager, removeStockController);
 
 export default stockRouter;
