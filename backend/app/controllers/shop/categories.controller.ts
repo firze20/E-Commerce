6882e2c138519c4b8f7f4e55b603905d77405df2
Domingues <domingues.fjp@hotmail.com>
2024-07-02 10:25:09 +0100

    Adding category cache

--- backend/app/controllers/shop/categories.controller.ts
+++ backend/app/controllers/shop/categories.controller.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from "express";
 import Category from "../../database/models/Category";
 
+import { getAsync, setAsync } from "../../utils/redis";
+
 const getAllCategories = async (req: Request, res: Response) => {
+
+  const cacheKey = `store/categories`;
+
   try {
+    // Check if the response is in the cache
+    const cashedData = await getAsync(cacheKey);
+    // If the response is in the cache, return it
+    if(cashedData) {
+      return res.status(200).json(JSON.parse(cashedData));
+    }
     const categories = await Category.findAll({
       attributes: ["id", "name", "description"],
     });
+
+    await setAsync(cacheKey, JSON.stringify(categories), 3600); // Cache the response for an hour
+
     res.status(200).json(categories);
   } catch (error) {
     res.status(500).json({ message: "Failed to retrieve categories" });
