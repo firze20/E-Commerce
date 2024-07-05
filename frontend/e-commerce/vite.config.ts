827b8252999b5ff48dd7cc38f4fd8c728e381b26
Domingues <domingues.fjp@hotmail.com>
2024-07-05 18:13:20 +0100

    update vite config and README

--- frontend/e-commerce/vite.config.ts
+++ frontend/e-commerce/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 3001,
+    host: true, // for external access
+    port: 3001, // for external access
   }
 })
