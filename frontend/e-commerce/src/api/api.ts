cc96e4db9a273e4590fc533385ca48b4c4bbbf1d
Domingues <domingues.fjp@hotmail.com>
2024-07-05 18:52:38 +0100

    ignoring pnpm-store

---
+++ frontend/e-commerce/src/api/api.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+// Default config for the axios instance 
+const axiosParams = {
+    // Set different base URL based on the environment
+    baseUrl: ""
+}
