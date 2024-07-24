23fe2851cb7c26a5c4f2a74f7367ca2b72a410c9
Domingues <domingues.fjp@hotmail.com>
2024-07-24 12:48:26 +0100

    removed cypress

--- frontend/e-commerce/cypress/e2e/firstTest.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('renders welcome message', () => {
-    cy.visit('http://localhost');
-
-    cy.get('[data-test-id="e-commerce-title"]').should('exist').should('have.text', 'E-Commerce');
-  })
-})
