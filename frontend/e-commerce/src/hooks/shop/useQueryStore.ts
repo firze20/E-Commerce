bf8e69a9393c2c300872d6c159f82f9cbe5eac8f
Domingues <domingues.fjp@hotmail.com>
2024-07-24 10:57:38 +0100

    use signal and add cache Time

--- frontend/e-commerce/src/hooks/shop/useQueryStore.ts
+++ frontend/e-commerce/src/hooks/shop/useQueryStore.ts
@@ -1,22 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchStore, StoreDataResponse } from "@/api/shop/storeApi";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { toast } from "react-toastify";
 
-
+/**
+ * Custom hook for querying store data.
+ * @param page - The page number for pagination.
+ * @param filters - The filters to apply to the query.
+ * @returns An object containing the queried store data and loading state.
+ */
 export const useQueryStore = (page: number, filters: Record<string, any>) => {
-  const params = { page, ...filters };
+
+  const params = useMemo(() => ({ page, ...filters }), [page, filters]);
   const { data, isLoading, isSuccess, isError } = useQuery<StoreDataResponse>({
     queryKey: ["store", params],
-    queryFn: () => fetchStore(params),
+    queryFn: ({ signal }) => fetchStore(params, { signal }),
+    refetchOnWindowFocus: false, // Prevents refetching when the window regains focus
+    staleTime: 1000 * 60 * 5, // 5 minutes
   });
 
   useEffect(() => {
-    if(isSuccess) {
-      toast.success("Store data fetched successfully 🏪", {
+    if (isSuccess) {
+      toast.success("Store data fetched successfully", {
         toastId: "store-data-toast",
       });
-    } else if(isError) {
+    } else if (isError) {
       toast.error("Error fetching store data", {
         toastId: "store-data-toast",
       });
