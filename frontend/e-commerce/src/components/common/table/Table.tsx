07ac28012572e82af04ca85c329dde4347b47f17
Domingues <domingues.fjp@hotmail.com>
2024-07-31 23:50:01 +0100

    fetching Purchases is working

--- frontend/e-commerce/src/components/common/table/Table.tsx
+++ frontend/e-commerce/src/components/common/table/Table.tsx
@@ -1,10 +1,14 @@
+import type { PurchaseResponse } from "@/api/shop/purchaseApi";
+
 type TableProps = {
   className?: string;
-  columns: string[];
-  data: any[];
+  data: PurchaseResponse["purchases"];
 };
 
-const Table = ({ columns, data, className }: TableProps) => {
+const Table = ({ data, className }: TableProps) => {
+  // If data is not empty, get columns from the keys of the first item
+  const columns = data.length > 0 ? Object.keys(data[0]) : [];
+
   return (
     <div className="overflow-x-auto">
       <table className={`table ${className}`}>
@@ -22,11 +26,13 @@ const Table = ({ columns, data, className }: TableProps) => {
         <tbody>
           {/* Table Rows */}
           {/* Table Cells */}
-          {data.map((row, index) => (
+          {/* {data.map((row, index) => (
             <tr>
-              <th>1</th>
+              <th>{row.id}</th>
+                <td>{row.createdAt}</td>
+                <td>{row.items.map((item) => item.name).join(", ")}</td>
             </tr>
-          ))}
+          ))} */}
           <tr>
             <td></td>
             <td></td>
--- frontend/e-commerce/src/views/purchases/MyPurchases.tsx
+++ frontend/e-commerce/src/views/purchases/MyPurchases.tsx
@@ -2,15 +2,26 @@ import { useQueryPurchases } from "@/hooks/purchases/useQueryPurchases";
 import Table from "@/components/common/table/Table";
 
 const MyPurchases = () => {
-
   const { data, isLoading, isSuccess, isError } = useQueryPurchases();
 
   return (
     <div>
-      Your purchase history
-      <Table columns={["Date", "Item", "Price"]} data={[]} />
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error fetching data</p>}
+      {isSuccess && data ? (
+        data.purchases.length > 0 ? (
+          <>
+            <h2>Your purchase history</h2>
+            <Table data={data.purchases} />
+          </>
+        ) : (
+          <p>Nothing to see here</p>
+        )
+      ) : (
+        !isLoading && !isError && <p>You need to sign up or sign in to see your purchases</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MyPurchases
+export default MyPurchases;
