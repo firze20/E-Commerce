7de9b44c0bde0dbe10571c3e0d850bcaab652d1d
Domingues <domingues.fjp@hotmail.com>
2024-08-07 12:13:50 +0100

    Stock working

--- frontend/e-commerce/src/views/manager/views/Stock.tsx
+++ frontend/e-commerce/src/views/manager/views/Stock.tsx
@@ -5,6 +5,7 @@ import LazySpinner from "@/components/common/loading/LazySpinner";
 import Error from "@/components/common/error/Error";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const Stock = () => {
   const { id } = useParams();
@@ -28,6 +29,14 @@ const Stock = () => {
   };
 
   const removeStock = () => {
+    if (quantityStock > data!.item.stock) {
+      // Display an error message or handle the error appropriately
+      toast.error("Cannot remove more stock than available", {
+        toastId: "remove-stock",
+      });
+      return;
+    }
+
     if (quantityStock > 0) {
       removeStockMutation.mutate({
         id: Number(id),
@@ -39,7 +48,7 @@ const Stock = () => {
   };
 
   return (
-    <div className="stats stats-vertical lg:stats-horizontal shadow">
+    <div className="stats stats-horizontal lg:stats-horizontal shadow">
       {isLoading && <LazySpinner show={isLoading} />}
       {isError || (error && <Error error={error} />)}
       {data && (
@@ -47,17 +56,42 @@ const Stock = () => {
           <div className="stat-title">{data.item.name}</div>
           <div className="stat-value">Stock: {data.item.stock}</div>
           <div className="stat-actions">
-            <button className="btn btn-accent" onClick={() => addStock()}>
-              Add Stock
-            </button>
-            <button className="btn btn-accent" onClick={() => removeStock()}>
-              Remove Stock
-            </button>
-            <input
-              type="number"
-              value={quantityStock}
-              onChange={(e) => setQuantityStock(Number(e.target.value))}
-            />
+            <div className="flex-col gap-3 mt-4 items-center">
+              <button
+                className="btn btn-success"
+                onClick={() => addStock()}
+                disabled={
+                  addStockMutation.isPending || removeStockMutation.isPending
+                }
+              >
+                {addStockMutation.isPending ? (
+                  <span className="loading loading-spinner text-success">
+                    Loading
+                  </span>
+                ) : (
+                  "Add Stock ➕"
+                )}
+              </button>
+              <button
+                className="btn btn-error"
+                onClick={() => removeStock()}
+                disabled={
+                  addStockMutation.isPending || removeStockMutation.isPending
+                }
+              >
+                {removeStockMutation.isPending ? (
+                  <span className="loading loading-spinner text-error"></span>
+                ) : (
+                  "Remove Stock ➖"
+                )}
+              </button>
+              <input
+                type="number"
+                className="input input-bordered"
+                value={quantityStock}
+                onChange={(e) => setQuantityStock(Number(e.target.value))}
+              />
+            </div>
           </div>
         </div>
       )}
